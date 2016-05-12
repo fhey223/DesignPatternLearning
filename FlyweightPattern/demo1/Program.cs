f426bb3e08facc4cefa84ba38959f412d271dfa7
Fhey <jie@fhey.me>
2016-05-12 17:58:06 +0800

    Signed-off-by: Fhey <jie@fhey.me>

---
+++ FlyweightPattern/demo1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyweightPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var extrinsicstate = 22;
+            FlyweightFactory f =new FlyweightFactory();
+
+            Flyweight fx = f.GetFlyweight("X");
+            fx.OPeration(--extrinsicstate);
+
+            Flyweight fy = f.GetFlyweight("Y");
+            fy.OPeration(--extrinsicstate);
+
+            Flyweight fz = f.GetFlyweight("Z");
+            fz.OPeration(--extrinsicstate);
+
+            UnshareConcreteFlyweight uf=new UnshareConcreteFlyweight();
+            uf.OPeration(--extrinsicstate);
+
+            Console.Read();
+        }
+    }
+}
