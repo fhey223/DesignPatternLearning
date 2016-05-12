08c6a09b04738b29b07a0d380e7085d9e7e2f3f4
Fhey <jie@fhey.me>
2016-05-12 11:24:00 +0800

    享元模式

--- FlyweightPattern/Properties/AssemblyInfo.cs
+++ FlyweightPattern/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("2508d2e3-c04a-45c3-8c6c-7858785f2522")]
+[assembly: Guid("24cb4426-d692-4aaf-a46f-506bbcaccecf")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
