d06016edb06f2660639e0229aedfc65fc86ad3c9
Fhey <jie@fhey.me>
2016-05-12 11:11:12 +0800

    Signed-off-by: Fhey <jie@fhey.me>

---
+++ FlyweightPattern/ConcreteFlyweight.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyweightPattern
+{
+
+    /// <summary>
+    /// TODOConcreteFlyweight
+    /// by gouyf
+    /// 2016/5/11 17:36:17
+    /// </summary>
+    public class ConcreteFlyweight
+    {
+    }
+}
