f1e7727677d5eb4c5ba732c498ab1d1cb8443a17
Fhey <jie@fhey.me>
2016-05-13 14:22:52 +0800

    创建解释器模式

---
+++ InterpreterPattern/AbsstractExpression.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterpreterPattern
+{
+
+    /// <summary>
+    /// TODOAbsstractExpression
+    /// by gouyf
+    /// 2016/5/12 18:21:17
+    /// </summary>
+    public abstract class AbsstractExpression
+    {
+        //public abstract void Interpret(Context context);
+    }
+}
