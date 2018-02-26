a67b7203e71481293e29f8398e0121c348aaf0d3
董炜杰 <dongwj5@centaline.com.cn>
2018-02-26 18:01:34 +0800

    修改单例模式demo

--- SingletonPattern2/Program.cs
+++ SingletonPattern2/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SingletonPattern2
+namespace SingletonPattern
 {
     class Program
     {
--- SingletonPattern2/Singleton.cs
+++ SingletonPattern2/Singleton.cs
@@ -4,42 +4,134 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SingletonPattern2
+namespace SingletonPattern
 {
-
-    /// <summary>
-    /// TODOSingleton
-    /// by gouyf
-    /// 2016/5/10 14:54:58
-    /// </summary>
-    public class Singleton
+    //第一种（懒汉，线程不安全）
+    public class Singleton1
     {
-        private static Singleton instance;
-        private  static readonly object syncRoot = new object();
-        private Singleton() { }
-        public static Singleton GetInstance()
+        private static Singleton1 instance;
+        private Singleton1() { }
+
+        public static Singleton1 GetInstance()
         {
             if (instance == null)
             {
-                 lock (syncRoot)
-                {
-                    if (instance == null)
-                    {
-                        instance = new Singleton();
-                    }
-                }
+                instance = new Singleton1();
             }
-           
             return instance;
         }
     }
-    public sealed class Singleton2
+
+    //第二种（懒汉，线程安全）：
+    public class Singleton2
     {
-        private static readonly Singleton2 instance = new Singleton2();
+        private static Singleton2 instance;
+
         private Singleton2() { }
+        private static readonly object _lock = new object();
         public static Singleton2 GetInstance()
         {
+            lock (_lock)
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton2();
+                }
+            }
+
             return instance;
+        }
+    }
+
+    //第三种（饿汉）：
+    public class Singleton3
+    {
+        private static Singleton3 instance = new Singleton3();
+        private Singleton3() { }
+        public static Singleton3 GetInstance()
+        {
+            return instance;
+        }
+    }
+
+    //第四种（静态内部类）：
+    public class Singleton4
+    {
+        private static class SingletonHolder
+        {
+            public static Singleton4 INSTANCE = new Singleton4();
+        }
+        private Singleton4() { }
+        public static Singleton4 GetInstance()
+        {
+            return SingletonHolder.INSTANCE;
+        }
+    }
+
+    //第五种（双重校验锁）：
+    public class Singleton5
+    {
+        private static Singleton5 instance;
+        private static readonly object _lock = new object();
+        protected Singleton5() { }
+        public static Singleton5 Instance
+        {
+            get
+            {
+                if (instance == null)
+                    lock (_lock)
+                        if (instance == null)
+                            instance = new Singleton5();
+                return instance;
+            }
+        }
+    }
+
+    //第六种 静态初始化
+    public sealed class Singleton6
+    {
+        private static readonly Singleton6 instance = null;
+        static Singleton6()
+        {
+            instance = new Singleton6();
+        }
+        private Singleton6()
+        {
+        }
+        public static Singleton6 Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+
+    //第七种 延迟初始化
+    public sealed class Singleton
+    {
+        private Singleton()
+        {
+        }
+        public static Singleton Instance
+        {
+            get
+            {
+                return Nested.instance;
+            }
+        }
+
+        public static void Hello()
+        {
+        }
+
+        private class Nested
+        {
+            internal static readonly Singleton instance = null;
+            static Nested()
+            {
+                instance = new Singleton();
+            }
         }
     }
 }
